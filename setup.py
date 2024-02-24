108c207659d5c04fb730363b08579eed53b81dfb
wei.zhong <wei.zhong@lge.com>
2024-02-23 23:42:28 -0500

    same with top-level setup.py

--- setup.py
+++ setup.py
@@ -79,6 +79,8 @@ if not SKIP_CUDA_BUILD:
             )
 
     cc_flag.append("-gencode")
+    cc_flag.append("arch=compute_61,code=sm_61")
+    cc_flag.append("-gencode")
     cc_flag.append("arch=compute_70,code=sm_70")
     cc_flag.append("-gencode")
     cc_flag.append("arch=compute_80,code=sm_80")
